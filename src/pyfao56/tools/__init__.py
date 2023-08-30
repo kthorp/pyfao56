a74f53fa0fd5cc2dcbbe4b268484016584b6bebd
Kelly Thorp <kelly.thorp@usda.gov>
2023-08-30 14:07:03 -0500

    Developed the visualization.py module

--- src/pyfao56/tools/__init__.py
+++ src/pyfao56/tools/__init__.py
@@ -11,11 +11,13 @@ The tools subpackage contains the following modules:
         and using measured volumetric soil water content data
 
 10/17/2022 Subpackage created by Josh Brekel
-11/09/2022 Initial Visualization scripts created by Josh Brekel (coming soon!)
+11/09/2022 Initial Visualization scripts created by Josh Brekel
 03/02/2023 SoilWater Class functions developed by Josh Brekel, USDA-ARS
 08/25/2023 Moved forecast.py from custom to tools
+08/29/2023 Added visualization.py
 ########################################################################
 """
 
 from .forecast import Forecast
 from .soil_water import SoilWaterSeries
+from .visualization import Visualization
---
+++ tests/test7/cornE42FF2023.py
@@ -0,0 +1,75 @@
+"""
+########################################################################
+The cornE42FF2023.py module contains a function to setup and run pyfao56
+for the (fully irrigated) E42 plot in a 2023 corn field study at the
+Limited Irrigation Research Farm (LIRF) in Greeley, Colorado.
+Specifically, this module tests the Visualization class in pyfao56.
+
+The cornE42FF2023.py module contains the following:
+    run - function to setup and run pyfao56 for the E42 plot in a 2023
+          corn field study in Greeley, Colorado.
+
+08/29/2023 Scripts developed for running pyfao56 for 2023 corn data
+########################################################################
+"""
+
+import pyfao56 as fao
+import pyfao56.tools as tools
+import os
+
+def run():
+    """Setup and run pyfao56 for a 2023 corn field study"""
+
+    #Get the module directory
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+
+    #Specify the model parameters from parameters file
+    par_file = os.path.join(module_dir, 'E42FF2023.par')
+    par = fao.Parameters()
+    par.loadfile(par_file)
+
+    #Specify the model weather data from weather file
+    wth_file = os.path.join(module_dir, 'LIRFWeather2023.wth')
+    wth = fao.Weather(wth_file)
+
+    #Specify the irrigation schedule from irrigation file
+    irr_file = os.path.join(module_dir, 'E42FF2023.irr')
+    irr = fao.Irrigation(irr_file)
+
+    #Specify the soil profile data from soil file
+    sol_file = os.path.join(module_dir, 'E42FF2023.sol')
+    sol = fao.SoilProfile(sol_file)
+
+    #Specify Kcb from overhead canopy images via an update file
+    upd_file = os.path.join(module_dir, 'E42FF2023.upd')
+    upd = fao.Update(upd_file)
+
+    #Run the model with Update and SoilProfile classes
+    mdl = fao.Model('2023-122', '2023-305', par, wth, irr=irr,
+                    sol=sol, upd=upd, cons_p=True)
+    mdl.run()
+    print(mdl)
+    mdl.savefile(os.path.join(module_dir,'E42FF2023.out'))
+
+    #Analyze measured soil water data
+    #Need to update soil water data with remainder of 2023 season.
+    sws = tools.SoilWaterSeries(filepath='E42FF2023.sws',
+                                par=par,sol=sol)
+    for key in sorted(sws.swdata.keys()):
+        print('Computing soil water depletion for {:s}'.format(key))
+        sws.swdata[key].getZr(mdl)
+        sws.swdata[key].computeDr()
+        sws.swdata[key].computeKs(mdl)
+    sws.savefile(os.path.join(module_dir,'E42FF2023.sws'))
+
+    #Plot measured and simulated data
+    vis = tools.Visualization(mdl, sws=sws, dayline=True)
+    pngpath = os.path.join(module_dir, 'E42FF2023_Dr.png')
+    vis.plot_Dr(drmax=True,raw=True,events=True,obs=True,ks=True,
+                dp=True,title='2023 Corn E42FF Dr',
+                filepath=pngpath)
+    pngpath = os.path.join(module_dir, 'E42FF2023_ET.png')
+    vis.plot_ET(title='2023 Corn E42FF ET', show=True, filepath=pngpath)
+
+if __name__ == '__main__':
+    run()
